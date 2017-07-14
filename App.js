8ed7b181612b43781788443f8244217317302130
BAAMOSK <bunth667@gmail.com>
2017-07-14 03:04:29 -0400

    firebase login

--- App.js
+++ App.js
@@ -1,22 +1,21 @@
 import React, { Component } from 'react';
-import { Text, View } from "react-native";
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducers from './src/reducers';
 import CONFIG from './config';
 import firebase from 'firebase';
 import LoginForm from './src/components/LoginForm';
+import ReduxThunk from 'redux-thunk';
 
 class App extends Component {
   componentDidMount() {
     firebase.initializeApp(CONFIG);
   }
   render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
     return (
-      <Provider store={createStore(reducers)}>
-        <View>
+      <Provider store={store}>
           <LoginForm />
-        </View>
       </Provider>
     );
   }
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,23 +1,49 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
+import { connect } from 'react-redux';
+import { emailChanged, passwordChanged, loginUser } from '../actions';
 import { FormLabel, FormInput, FormValidationMessage } from 'react-native-elements';
 import Submit from './Submit';
 
 class LoginForm extends Component {
+  onEmailChange(text) {
+    //redux function takes React-text
+    this.props.emailChanged(text);
+  }
+  onPasswordChange(text) {
+    this.props.passwordChanged(text);
+  }
+
+  onSubmit() {
+    const { email, password } = this.props;
+    this.props.loginUser({ email, password});
+  }
+
   render() {
+    console.log(this.props.password);
     return(
       <View>
         <FormLabel>Email</FormLabel>
         {/* placeholder="you@gmail.com"*/}
-        <FormInput ref='forminput' textInputRef='email' />
+        <FormInput onChangeText={this.onEmailChange.bind(this)} />
 
         <FormLabel>Password</FormLabel>
-        <FormInput ref='forminput' textInputRef='password' />
+        <FormInput onChangeText={this.onPasswordChange.bind(this)} />
         {/* <Text securityTextEntry > </Text> */}
-        <Submit />
+        <Button title="Submit" onPress={this.onSubmit.bind(this)}>Submit</Button>
+        {/*<Submit onPress={this.onSubmit.bind(this)} />*/}
       </View>
     );
   }
 }
 
-export default LoginForm;
+const mapStateToProps = ({ auth }) => {
+  const { email, password } = auth;
+  return {
+    email,
+    password
+  };
+};
+
+//Redux then React
+export default connect(mapStateToProps, { emailChanged, passwordChanged, loginUser })(LoginForm);
